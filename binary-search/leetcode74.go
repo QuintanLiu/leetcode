f5186d5b270239eb901fb864368356cf4ccbe4ee
‘zhame <l>
2022-11-27 21:06:38 +0800

    feat:leetcode74

---
+++ binary-search/leetcode74.go
@@ -0,0 +1,15 @@
+package binary_search
+
+func searchMatrix(matrix [][]int, target int) bool {
+	i, j := len(matrix)-1, 0
+	for i >= 0 && j < len(matrix[0]) {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] < target {
+			j++
+		} else {
+			i--
+		}
+	}
+	return false
+}
